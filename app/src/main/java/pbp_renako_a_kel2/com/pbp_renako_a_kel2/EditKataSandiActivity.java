b4ee206795c3099a036f95514f4fe8b9747fe644
sintaxd <jasintaxd@yahoo.com>
2018-09-06 21:40:38 +0700

    register updated

---
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/EditKataSandiActivity.java
@@ -0,0 +1,13 @@
+package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditKataSandiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_kata_sandi);
+    }
+}
