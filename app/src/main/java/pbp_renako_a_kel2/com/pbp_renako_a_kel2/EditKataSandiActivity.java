f750c7b744ed1b9411922a6e5fbcbc7b6fb09e24
sintaxd <jasintaxd@yahoo.com>
2018-09-08 11:58:01 +0700

    tombol back di edit kata sandi

--- app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/EditKataSandiActivity.java
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/EditKataSandiActivity.java
@@ -2,6 +2,7 @@ package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EditKataSandiActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class EditKataSandiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_kata_sandi);
+
+        getSupportActionBar().setTitle("Ubah Kata Sandi");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/PengaturanActivity.java
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/PengaturanActivity.java
@@ -1,7 +1,9 @@
 package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PengaturanActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class PengaturanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengaturan);
+    /*
+        belum bisa dibikin, soalnya main menunya blm ada
+
+        getSupportActionBar().setTitle("Pengaturan");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    */
+    }
+    public void txtUbahKataSandi_Pengaturan(View view)
+    {
+        startActivity(new Intent(this,EditKataSandiActivity.class));
     }
 }
