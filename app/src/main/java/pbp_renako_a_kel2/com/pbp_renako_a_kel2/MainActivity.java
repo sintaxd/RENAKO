518e1565b8d46eb9adbce2e13244f0e0d2d87ae3
Suma-Pc <sumarianakadek@gmail.com>
2018-09-08 11:49:07 +0700

    udah mau di run ya

--- app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/MainActivity.java
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/MainActivity.java
@@ -1,16 +1,39 @@
 package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //hai geo
-    // hai ganteng :*
-    //3333
+    private EditText email,pass;
+    private Button login;
+    private TextView reg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        email=(EditText) findViewById(R.id.txtEmail_Login);
+        pass=(EditText) findViewById(R.id.txtKataSandi_Login);
+        login=(Button) findViewById(R.id.btnLogin_Login);
+        reg=(TextView) findViewById(R.id.txtRegister_Login);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
 }
