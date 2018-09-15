b154c1512c46ac59a920d766e60bff082624ab71
Suma-Pc <sumarianakadek@gmail.com>
2018-09-15 23:14:51 +0700

    ini main menu udah ada app drawer, tp contentnya blum aku isi karena gak cocok katanya temanya

---
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/main_menu.java
@@ -0,0 +1,35 @@
+package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class main_menu extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer=findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawer,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
