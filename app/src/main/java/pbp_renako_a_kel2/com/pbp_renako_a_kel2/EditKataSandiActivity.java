19b8e5c1c8587027e6e1fe12ac2a5a3319c005b9
bananalord <dannyandryanto@gmail.com>
2018-09-22 13:03:35 +0700

    nambah activity doang

--- app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/EditKataSandiActivity.java
+++ app/src/main/java/pbp_renako_a_kel2/com/pbp_renako_a_kel2/EditKataSandiActivity.java
@@ -1,10 +1,20 @@
 package pbp_renako_a_kel2.com.pbp_renako_a_kel2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditKataSandiActivity extends AppCompatActivity {
+
+    private EditText eKataSandi;
+    private Button mbtnSimpan_EKS, mBtnBatal_EKS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,37 @@ public class EditKataSandiActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Ubah Kata Sandi");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setAtribut();
+        mbtnSimpan_EKS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick(mbtnSimpan_EKS);
+            }
+        });
+        mBtnBatal_EKS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(EditKataSandiActivity.this, PengaturanActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+    private void setAtribut(){
+        eKataSandi = findViewById(R.id.editText6);
+        mbtnSimpan_EKS = findViewById(R.id.btnSimpan_EKS);
+        mBtnBatal_EKS = findViewById(R.id.btnBatal_EKS);
     }
 }
+
+
+
+
+
+
+
+
+
+
